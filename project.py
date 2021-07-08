0a779cc645b5daa72d8f81fdc27d03fd570cf3a8
trai <traianmail@gmail.com>
2021-07-09 01:17:53 +0300

    start of file

---
+++ project.py
@@ -0,0 +1,11 @@
+"""This is project for algorithmic thinking 2, week 4 """
+
+def build_scoring_matrix(alphabet, diag_score, off_diag_score, dash_score):
+    """Takes as input a set of characters alphabet and three scores
+    diag_score, off_diag_score, and dash_score. The function return
+    s a dictionary of dictionaries whose entries are indexed by pair
+    s of characters in alphabet plus '-'. The score for any entry indexed 
+    by one or more dashes is dash_score. The score for the remaining
+    diagonal entries is diag_score. Finally, the score for the remaining 
+    off-diagonal entries is off_diag_score."""
+    return None
