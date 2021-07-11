cb16dcab82e070572e5cc062050d83c85461a08e
horei <traianmail@gmail.com>
2021-07-12 01:47:29 +0300

    working on second function

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 """This is project for algorithmic thinking 2, week 4
 PYTHON3 is used."""
-example = "set(['A', 'C', 'T', 'G']), 6, 2, -4 "
+example = "compute_alignment_matrix('A', 'A', {'A': {'A': 6, 'C': 2, '-': -4, 'T': 2, 'G': 2}, 'C': {'A': 2, 'C': 6, '-': -4, 'T': 2, 'G': 2}, '-': {'A': -4, 'C': -4, '-': -4, 'T': -4, 'G': -4}, 'T': {'A': 2, 'C': 2, '-': -4, 'T': 6, 'G': 2}, 'G': {'A': 2, 'C': 2, '-': -4, 'T': 2, 'G': 6}}, True) expected [[0, -4], [-4, 6]] but received [[0]] "
 
 
 
@@ -28,3 +28,28 @@ def build_scoring_matrix(alphabet, diag_score, off_diag_score, dash_score):
 
 
     return answer_dict
+
+
+def compute_alignment_matrix(seq_x, seq_y, scoring_matrix, global_flag):
+    """Takes as input two sequences seq_x and seq_y whose elements share 
+    a common alphabet with the scoring matrix scoring_matrix. The function
+    computes and returns the alignment matrix for seq_x and seq_y as described
+    in the Homework. If global_flag is True, each entry of the alignment
+    matrix is computed using the method described in Question 8 of the
+    Homework. If global_flag is False, each entry is computed using the 
+    method described in Question 12 of the Homework."""
+
+    length_m = len(seq_x)
+    length_n = len(seq_y)
+
+    matrix = list()
+    matrix.append(list())
+    matrix[0].append(0)
+
+    for index_i in range(1,length_m+1):
+        matrix.append(list())
+
+
+
+
+    return matrix
